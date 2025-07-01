542e02af6276d956101e22d6a2832a2fd332df0f
Javier Beltran <170134446+JavierBeltranHMO@users.noreply.github.com>
2025-06-30 17:01:05 -0700

    navmenu scrollspy

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
-/**
- * Header toggle
- */
+//Header toggle
 const headerToggleBtn = document.querySelector(".header-toggle");
 
 function headerToggle() {
@@ -10,9 +8,7 @@ function headerToggle() {
 }
 headerToggleBtn.addEventListener("click", headerToggle);
 
-/**
- * Hide mobile nav on same-page/hash links
- */
+//Hide mobile nav on same-page/hash links
 document.querySelectorAll("#navmenu a").forEach((navmenu) => {
   navmenu.addEventListener("click", () => {
     if (document.querySelector(".header-show")) {
@@ -20,3 +16,28 @@ document.querySelectorAll("#navmenu a").forEach((navmenu) => {
     }
   });
 });
+
+//Navmenu Scroll
+let navmenulinks = document.querySelectorAll(".navmenu a");
+
+function navmenuScrollspy() {
+  navmenulinks.forEach((navmenulink) => {
+    if (!navmenulink.hash) return;
+    let section = document.querySelector(navmenulink.hash);
+    if (!section) return;
+    let position = window.scrollY + 200;
+    if (
+      position >= section.offsetTop &&
+      position <= section.offsetTop + section.offsetHeight
+    ) {
+      document
+        .querySelectorAll(".navmenu a.active")
+        .forEach((link) => link.classList.remove("active"));
+      navmenulink.classList.add("active");
+    } else {
+      navmenulink.classList.remove("active");
+    }
+  });
+}
+window.addEventListener("load", navmenuScrollspy);
+document.addEventListener("scroll", navmenuScrollspy);
